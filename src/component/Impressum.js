8978ace871a88cc1fcd1ab3b402f15232f62261b
knuspersnack <thumos@posteo.gr>
2021-01-02 12:55:00 +0100

    Testing the deployment

--- src/component/Impressum.js
+++ src/component/Impressum.js
@@ -4,7 +4,7 @@ class Impressum extends React.Component {
     render() {
         return (
             <div className='impressum'>
-                <h1>Impressum</h1>
+                <h1>Impressum 1</h1>
                 <p>Work in progess...</p>
             </div>
         )
