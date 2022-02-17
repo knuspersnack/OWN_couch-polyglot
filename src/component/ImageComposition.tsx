3d8b565bb40bb5f099ce97a22fd1c1005fa9f0a6
knuspersnack <geo.d@posteo.de>
2022-02-17 23:32:53 +0100

    About Section finished

--- src/component/ImageComposition.tsx
+++ src/component/ImageComposition.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import classNames from 'classnames';
 import nat1 from '../assets/natours/nat-1-large.jpg';
 import nat2 from '../assets/natours/nat-2-large.jpg';
 import nat3 from '../assets/natours/nat-3-large.jpg';
 import style from '../style/ui-components/ImageComposition.module.scss';
+var cx = classNames.bind(style);
 
 type Props = {};
 
 const ImageComposition = (props: Props) => {
   return (
     <div className={style.composition}>
-      <img src={nat1} alt='' className={style.photo} />
-      <img src={nat2} alt='' className={style.photo} />
-      <img src={nat3} alt='' className={style.photo} />
+      <img src={nat1} alt='' className={cx(style.photo, style.pos1)} />
+      <img src={nat2} alt='' className={cx(style.photo, style.pos2)} />
+      <img src={nat3} alt='' className={cx(style.photo, style.pos3)} />
     </div>
   );
 };
